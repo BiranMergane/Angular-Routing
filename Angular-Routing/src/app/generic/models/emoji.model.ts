47f0f39e982e61f540c8fe980c3d56631a2aefe4
BiranMergane <159021614+BiranMergane@users.noreply.github.com>
2025-09-29 10:42:09 +0000

    Prima di iniziare parte 2

---
+++ Angular-Routing/src/app/generic/models/emoji.model.ts
@@ -0,0 +1,64 @@
+export interface Emoji
+{
+  name : string;
+  emoji : string;
+};
+
+export const FRUITS = [
+    { name: 'Uva', emoji: '🍇' },
+    { name: 'Melone', emoji: '🍈' },
+    { name: 'Anguria', emoji: '🍉' },
+    { name: 'Arancia', emoji: '🍊' },
+    { name: 'Limone', emoji: '🍋' },
+    { name: 'Banana', emoji: '🍌' },
+    { name: 'Ananas', emoji: '🍍' },
+    { name: 'Mela Rossa', emoji: '🍎' },
+    { name: 'Mela Verde', emoji: '🍏' },
+    { name: 'Pera', emoji: '🍐' },
+    { name: 'Pesca', emoji: '🍑' },
+    { name: 'Ciliegia', emoji: '🍒' },
+  ];
+
+ export const  ANIMALS = [
+    { name: 'Scimmia', emoji: '🐒' },
+    { name: 'Cane', emoji: '🐶' },
+    { name: 'Lupo', emoji: '🐺' },
+    { name: 'Gatto', emoji: '🐱' },
+    { name: 'Tigre', emoji: '🐅' },
+    { name: 'Leopardo', emoji: '🐆' },
+    { name: 'Cavallo', emoji: '🐎' },
+    { name: 'Toro', emoji: '🐂' },
+    { name: 'Mucca', emoji: '🐄' },
+    { name: 'Maiale', emoji: '🐷' },
+    { name: 'Cinghiale', emoji: '🐗' },
+    { name: 'Ariete', emoji: '🐏' },
+    { name: 'Pecora', emoji: '🐑' },
+    { name: 'Capra', emoji: '🐐' },
+    { name: 'Cammello', emoji: '🐪' },
+    { name: 'Elefante', emoji: '🐘' },
+    { name: 'Topolino', emoji: '🐭' },
+    { name: 'Topo', emoji: '🐀' },
+    { name: 'Coniglio', emoji: '🐰' },
+    { name: 'Orso', emoji: '🐻' },
+    { name: 'Koala', emoji: '🐨' },
+    { name: 'Panda', emoji: '🐼' },
+    { name: 'Gallo', emoji: '🐓' },
+    { name: 'Pulcino', emoji: '🐥' },
+    { name: 'Pinguino', emoji: '🐧' },
+    { name: 'Rana', emoji: '🐸' },
+    { name: 'Coccodrillo', emoji: '🐊' },
+    { name: 'Tartaruga', emoji: '🐢' },
+    { name: 'Serpente', emoji: '🐍' },
+    { name: 'Drago', emoji: '🐉' },
+    { name: 'Balena', emoji: '🐳' },
+    { name: 'Delfino', emoji: '🐬' },
+    { name: 'Pesce', emoji: '🐟' },
+    { name: 'Seppia', emoji: '🐙' },
+    { name: 'Paguro', emoji: '🐚' },
+    { name: 'Lumaca', emoji: '🐌' },
+    { name: 'Bruco', emoji: '🐛' },
+    { name: 'Formica', emoji: '🐜' },
+    { name: 'Ape', emoji: '🐝' },
+    { name: 'Coccinella', emoji: '🐞' }
+  ];
+
