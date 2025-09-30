28784b79dfdc2a642295b1618cd492434b55ee41
BiranMergane <159021614+BiranMergane@users.noreply.github.com>
2025-09-30 08:51:59 +0000

    Fine Esercizio

--- Angular-Routing/src/app/app.routes.ts
+++ Angular-Routing/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AnimalsComponent } from './animals/animals.component';
 import { FruitsComponent } from './fruits/fruits.component';
 import { NgModule } from '@angular/core';
+import { GenericComponent } from './generic/generic.component';
 
 export const routes: Routes = [
   { path: 'animals', component: AnimalsComponent},
-  { path: 'fruits', component: FruitsComponent}
+  { path: 'fruits', component: FruitsComponent},
+  { path: 'generic/:id', component: GenericComponent }
 ];
 
 @NgModule({
--- Angular-Routing/src/app/generic/generic.component.ts
+++ Angular-Routing/src/app/generic/generic.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ANIMALS, Emoji, FRUITS } from './models/emoji.model';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-generic',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './generic.component.html',
   styleUrl: './generic.component.css'
 })
-export class GenericComponent {
-
+export class GenericComponent{
+  //Crea un vettore di tipo Emoji e lo inizializza
+  genVect : Emoji[]= [{name:"", emoji:""}];
+  //Riceve l'oggetto ActivatedRoute come dependency injection
+  constructor(private route: ActivatedRoute) {
+    //Gestisce i cambi di route con l'observable paramMap
+    this.route.paramMap.subscribe(this.getRouterParam);
+  }
+ 
+  //Ogni volta che viene invocata la route tracks/:id, l'observable paramMap richiama questo metodo
+  getRouterParam = (params: ParamMap) =>
+  {
+    let uri_param = params.get('id'); //Ottengo l'id dalla ParamMap
+    console.log (uri_param); //Stampo su console
+    //this.service.getTrack()
+    if (uri_param == 'fruits') this.genVect = FRUITS;
+    if (uri_param == 'animals') this.genVect = ANIMALS;
+  }
 }
